e722feeb703a7b0e9560a15e3b178f60951aec11
tp00012x <anthony_torres01@yahoo.com>
2017-09-27 18:13:10 -0500

    Added our first viewset and router

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
-from .views import HelloApiView
+from django.conf.urls import url, include
+from .views import HelloApiView, HelloViewSet
+
+from rest_framework.routers import DefaultRouter
 
 app_name = 'profiles_api'
+router = DefaultRouter()
+router.register('hell-viewset', HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     url(r'^hello-view/', HelloApiView.as_view()),
+    url(r'', include(router.urls))
 ]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import HelloSerializer
 # Create your views here.
 
+
 class HelloApiView(APIView):
+    '''Test Api view.'''
+
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         '''Returns a list of APIView features'''
@@ -16,3 +24,44 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        '''Create a hello message with out name.'''
+
+        serializer = HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        '''Handles updating an object.'''
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        '''Path request, only updates fields provide in the request.'''
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+
+        return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    '''Test API View set.'''
+
+    def list(self, request):
+        '''Return a hello message'''
+
+        a_viewset = [
+            'Uses actions (list, create retrieve, update, partial_update',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code.',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
